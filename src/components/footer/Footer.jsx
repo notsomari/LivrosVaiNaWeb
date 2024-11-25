65d156daff56d085b48b59329b1bb359b31c36d3
Mariana Miranda <marianamiranda.ms@gmail.com>
2024-11-25 15:35:10 -0300

    Adicionando nova sessão do Início e Footer, além da mudança de nome do site no index.

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import facebookLogo from '../../assets/facebookLogo.png'
+import twitterLogo from '../../assets/twitterLogo.png'
+import youtubeLogo from '../../assets/youtubeLogo.png'
+import linkedinLogo from '../../assets/linkedinLogo.png'
+import instagramLogo from '../../assets/instagramLogo.png'
+import s from './footer.module.scss'
+
+
+export default function Footer(){
+    return(
+        <footer>
+            <section className={s.contatosFooter}>
+            <p>4002-8922</p>
+            <nav>
+                <a href=""><img src={facebookLogo} alt="Imagem que ilustra a logo do Facebook." /></a>
+                <a href=""><img src={twitterLogo} alt="Imagem que ilustra a logo do Twitter." /></a>
+                <a href=""><img src={youtubeLogo} alt="Imagem que ilustra a logo do Youtube." /></a>
+                <a href=""><img src={linkedinLogo} alt="Imagem que ilustra a logo do Linkedin." /></a>
+                <a href=""><img src={instagramLogo} alt="Imagem que ilustra a logo do Instagram." /></a>
+            </nav>
+            </section>
+        </footer>
+    )
+}
--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,11 +1,36 @@
 import s from './inicio.module.scss'
-
+import balança from '../../assets/balança.png'
+import comunidade from '../../assets/comunidade.png'
+import lendoLivro from '../../assets/lendoLivro.png'
+import transformar from '../../assets/transformar.png'
 
 export default function Inicio (){
     return (
        <main>
         <section className={s.primeiraSessaoInicio}>
             <h2>Venha Fazer Parte Da Maior Rede De Doação.</h2>
+        </section>
+
+        <section className={s.porqueDoarSection}>
+            <h2>Por que Devo Doar?</h2>
+        <section className={s.containerCards}>
+            <section>
+                <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+                <img src={comunidade} alt="Imagem ilustrando círculo social de 4 pessoas." />
+            </section>
+            <section>
+                <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+                <img src={lendoLivro} alt="Imagem ilustrando pessoa lendo." />
+            </section>
+            <section>
+                <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+                <img src={transformar} alt="Imagem ilustrando 6 pessoas transformando o mundo." />
+            </section>
+            <section>
+                <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+                <img src={balança} alt="Imagem ilustrando uma balança que representa igualdade." />
+            </section>
+            </section>
         </section>
        </main>
     )
